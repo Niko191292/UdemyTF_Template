5ff5e03f7e77ee6accbcf9dd33f5ca31237354be
Niko191292 <75900081+Niko191292@users.noreply.github.com>
2021-07-01 15:17:33 +0200

    up

---
+++ Chapter4_NN/Chapter4_3_DeepLearningVocabulary/oneHotIntuition.py
@@ -0,0 +1,48 @@
+from typing import Tuple
+import numpy as np
+
+
+def get_dataset() -> Tuple[np.ndarray, np.ndarray]:
+    x = np.array([[0, 0], [1, 0], [0, 1], [1, 1]])
+    y = np.array([[0], [1], [1], [1]])
+    return x, y
+
+
+def to_categorical(y: np.ndarray, num_classes: int) -> np.ndarray:
+    y_categorical = np.zeros(shape=(len(y), num_classes))
+    for i, yi in enumerate(y):
+        y_categorical[i, yi] = 1
+    return y_categorical
+
+
+# Umsetzung der Softmax Formel
+def softmax(y_pred: np.ndarray) -> np.ndarray:
+    probabilities = np.zeros_like(y_pred)
+    for i in range(len(y_pred)):
+        exps = np.exp(y_pred[i])
+        probabilities[i] = exps / np.sum(exps)
+    return probabilities
+
+# Bildet die Fehlerfunktion
+def cross_entropy(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    num_samples = y_true.shape[0]
+    loss = -np.sum(y_true * np.log(y_pred)) / num_samples
+    return loss
+
+
+if __name__ == "__main__":
+    x, y = get_dataset()
+    print(y)
+
+    y_true = to_categorical(y, num_classes=2)
+    print(y_true)
+
+    y_logits = np.array([[10.8, -3.3], [12.2, 21.8], [1.1, 4.9], [7.05, 3.95]])
+
+    # Berechnet die Wahrscheinlichkeiten, Summe immer == 1
+    y_pred = softmax(y_logits)
+    print(y_pred)
+
+    #Wahrscheinlichkeitsverteilung vergleichen
+    loss = cross_entropy(y_true, y_pred)
+    print(loss)
