c7334a87d38d4b823ed85123979a7a3a3a66e324
Niko191292 <75900081+Niko191292@users.noreply.github.com>
2021-07-01 16:00:52 +0200

    up

--- Chapter5_DNN/Chapter5_1_XORClassification/xorDnn.py
+++ Chapter5_DNN/Chapter5_1_XORClassification/xorDnn.py
@@ -3,7 +3,6 @@ import numpy as np
 from typing import Tuple
 
 
-
 def get_dataset() -> Tuple[np.ndarray, np.ndarray]:
     """XOR - Dataset"""
     x = np.array([[0, 0], [1, 0], [0, 1], [1, 1]])
@@ -13,9 +12,16 @@ def get_dataset() -> Tuple[np.ndarray, np.ndarray]:
 
 class Model:
     def __init__(self, optimizer, loss, metric) -> None:
+        # Model parameters
         self.optimizer = optimizer
         self.loss = loss
         self.metric = metric
+        # Weights
+        self.W1 = tf.Variable(tf.random.truncated_normal(shape=[], stddev=0.1), name="W1")
+        self.W2 = tf.Variable(tf.random.truncated_normal(shape=[], stddev=0.1), name="W2")
+        # Biases
+        self.b1 = tf.Variable(tf.constant(0.0, shape=[]), name="b1")
+        self.b2 = tf.Variable(tf.constant(0.0, shape=[]), name="b2")
 
     def _update_weights(self, x: np.ndarray, y: np.ndarray, y_pred: np.ndarray) -> None:
         pass
@@ -37,11 +43,12 @@ if __name__ == "__main__":
     num_feature = 2
     num_targets = 1
     learning_rate = 0.01
+    units_list = [num_feature, 6, num_targets]
 
     optimizer = tf.optimizers.Adam(learning_rate=learning_rate)
     loss = tf.keras.losses.MeanAbsoluteError()
     metric = tf.keras.metrics.BinaryAccuracy()
 
-    p = Model(optimizer, loss, metric)
-    p.train(x, y, epochs = 10)
+    model = Model(optimizer, loss, metric)
+    model.fit(x, y, epochs = 10)
 
