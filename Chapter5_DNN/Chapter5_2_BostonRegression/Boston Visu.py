9bf9b01b7edf7f0d1d09bdc80efca97c6e33b97c
Nikolai Isken <nikolai.isken@stud.hs-emden-leer.de>
2021-07-02 15:28:13 +0200

    up

--- Chapter5_DNN/Chapter5_2_BostonRegression/Boston Visu.py
+++ Chapter5_DNN/Chapter5_2_BostonRegression/Boston Visu.py
@@ -1,6 +1,6 @@
 import pandas as pd # Excel für Python
 from sklearn.datasets import load_boston
-
+import matplotlib.pyplot as plt
 
 dataset = load_boston()
 x = dataset.data
@@ -10,3 +10,8 @@ df = pd.DataFrame(x, columns=dataset.feature_names)
 df["y"] = y
 
 print(df.head(n=20))
+print(df.info())
+print(df.describe())
+
+df.hist(bins=30, figsize=(15, 15))
+plt.show()
